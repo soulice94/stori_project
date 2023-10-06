ab804bdf4eb2b1c41ca4fa81ca9383af6dc98116
Pedro Arturo Flores Ramirez <pepito_nx@hotmail.com>
2023-10-06 13:59:20 -0600

    first version lambda

--- lambda-python3.11/stori/app.py
+++ lambda-python3.11/stori/app.py
@@ -1,10 +1,10 @@
 import json
+import csv
+from decimal import *
 
 # import requests
 
-
-def lambda_handler(event, context):
-    """Sample pure Lambda function
+"""Sample pure Lambda function
 
     Parameters
     ----------
@@ -23,20 +23,66 @@ def lambda_handler(event, context):
     API Gateway Lambda Proxy Output Format: dict
 
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
+"""
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
 
-    #     raise e
+def lambda_handler(event, context):
+
+    months = {
+        '1': 'January',
+        '2': 'February',
+        '3': 'March',
+        '4': 'April',
+        '5': 'May',
+        '6': 'June',
+        '7': 'July',
+        '8': 'August',
+        '9': 'September',
+        '10':'October',
+        '11':'November',
+        '12':'December'
+    }
+
+    with open('txns.csv') as csvfile:
+        read_csv = csv.reader(csvfile, delimiter=',')
+        line_count = 0
+        transactions = {};
+        total_balance = Decimal(0)
+        average_debit_amount = Decimal(0)
+        average_credit_amount = Decimal(0)
+        count_debit = 0
+        count_credit = 0
+
+        for row in read_csv:
+            if line_count > 0:
+                date = row[1].split('/')
+                transaction = Decimal(row[2])
+                month = months[date[0]]
+                value = transactions.get(month)
+                if (value):
+                    transactions[month] = value + 1
+                else:
+                    transactions[month] = 1
+                if (transaction < 0):
+                    average_debit_amount += transaction
+                    count_debit += 1
+                else:
+                    average_credit_amount += transaction
+                    count_credit += 1
+                total_balance += transaction
+            line_count += 1
+        
+        print(total_balance)
+        print(average_credit_amount/count_credit)
+        print(average_debit_amount/count_debit)
+        print(transactions)
+
 
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world",
+            "total_balance": str(total_balance),
             # "location": ip.text.replace("\n", "")
         }),
     }
--- lambda-python3.11/tests/unit/test_handler.py
+++ lambda-python3.11/tests/unit/test_handler.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from hello_world import app
+from stori import app
 
 
 @pytest.fixture()
