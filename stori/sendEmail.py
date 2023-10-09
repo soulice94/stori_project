f46ef33aee6148f9b91ce4d787e20cb03bf85861
Pedro Arturo Flores Ramirez <pepito_nx@hotmail.com>
2023-10-09 00:57:41 -0600

    more clean up

--- stori/sendEmail.py
+++ stori/sendEmail.py
@@ -12,11 +12,6 @@ def send(data):
     # is still in the sandbox, this address must be verified.
     RECIPIENT = "pepitonx@gmail.com"
 
-    # Specify a configuration set. If you do not want to use a configuration
-    # set, comment the following variable, and the 
-    # ConfigurationSetName=CONFIGURATION_SET argument below.
-    # CONFIGURATION_SET = "ConfigSet"
-
     # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
     AWS_REGION = "us-east-1"
 
@@ -28,19 +23,6 @@ def send(data):
                 "This email was sent with Amazon SES using the "
                 "AWS SDK for Python (Boto)."
                 )
-                
-    # The HTML body of the email.
-    # BODY_HTML = """<html>
-    # <head></head>
-    # <body>
-    # <h1>Amazon SES Test (SDK for Python)</h1>
-    # <p>This email was sent with
-    #     <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-    #     <a href='https://aws.amazon.com/sdk-for-python/'>
-    #     AWS SDK for Python (Boto)</a>.</p>
-    # </body>
-    # </html>
-    #             """
 
     FILE = open("template.html", "r")
     BODY_HTML = FILE.read()
@@ -82,11 +64,8 @@ def send(data):
                 },
             },
             Source=SENDER,
-            # If you are not using a configuration set, comment or delete the
-            # following line
-            # ConfigurationSetName=CONFIGURATION_SET,
+
         )
-        # return response
     # Display an error if something goes wrong.	
     except ClientError as e:
         print(e.response['Error']['Message'])
